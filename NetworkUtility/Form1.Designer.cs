8076f480cbb5b9e08e4b1a86c0e2fe9908c37918
Dima <dima-bubnovskyi@gmail.com>
2017-04-15 16:33:00 +0300

    add menu

--- NetworkUtility/Form1.Designer.cs
+++ NetworkUtility/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NetworkUtility
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.цуйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.йуцToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ййййййуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.налаштуванняToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.допомогаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.налаштуванняToolStripMenuItem,
+            this.допомогаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.цуйToolStripMenuItem,
+            this.йуцToolStripMenuItem,
+            this.ййййййуToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // цуйToolStripMenuItem
+            // 
+            this.цуйToolStripMenuItem.Name = "цуйToolStripMenuItem";
+            this.цуйToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.цуйToolStripMenuItem.Text = "цуй";
+            // 
+            // йуцToolStripMenuItem
+            // 
+            this.йуцToolStripMenuItem.Name = "йуцToolStripMenuItem";
+            this.йуцToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.йуцToolStripMenuItem.Text = "йуц";
+            // 
+            // ййййййуToolStripMenuItem
+            // 
+            this.ййййййуToolStripMenuItem.Name = "ййййййуToolStripMenuItem";
+            this.ййййййуToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.ййййййуToolStripMenuItem.Text = "ййййййу";
+            // 
+            // налаштуванняToolStripMenuItem
+            // 
+            this.налаштуванняToolStripMenuItem.Name = "налаштуванняToolStripMenuItem";
+            this.налаштуванняToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.налаштуванняToolStripMenuItem.Text = "Налаштування";
+            // 
+            // допомогаToolStripMenuItem
+            // 
+            this.допомогаToolStripMenuItem.Name = "допомогаToolStripMenuItem";
+            this.допомогаToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.допомогаToolStripMenuItem.Text = "Допомога";
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(438, 515);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(584, 532);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "NetworkUtility";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem налаштуванняToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem допомогаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem цуйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem йуцToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ййййййуToolStripMenuItem;
     }
 }
 
--- NetworkUtility/Form1.cs
+++ NetworkUtility/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace NetworkUtility
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
         }
     }
 }
